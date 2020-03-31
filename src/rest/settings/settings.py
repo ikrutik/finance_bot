5a2e8d8ca7fbfa55e58fdae2766b076761429326
ikrutik <ks@utair.digital>
2020-03-31 23:44:48 +0300

    git init

---
+++ src/rest/settings/settings.py
@@ -0,0 +1,11 @@
+import os
+
+PROJECT_ROOT = os.path.abspath(os.path.join(__file__, "../../../../"))
+SRC_ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, "src"))
+
+# Параметры запуска приложения
+HOST = "127.0.0.1"
+PORT = 8081
+DEBUG = True
+
+SSL_CONTEXT = None
